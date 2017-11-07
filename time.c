31e8c8711e91c2a517f068cf0962f2532a1534d6
miladalikhani <miladalikhani76@gmail.com>
2017-11-07 20:39:05 +0330

    add comment to time.c

--- time.c
+++ time.c
@@ -3,10 +3,10 @@
 
 int main()
 {
-	time_t rawtime;
-	struct tm * timeinfo;
-	time ( &rawtime );
-	timeinfo = localtime ( &rawtime);
-	printf ( "current local time and data : %s" , asctime ( timeinfo ) );
+	time_t seconds; 
+	struct tm * timeStruct; // structure for saving calendar time data
+	time ( &seconds ); // seconds is equal to seconds since the Epoch 1970-01-01 00:00:00 +0000 UTC
+	timeStruct = localtime ( &seconds ); // break down seconds to calendar time
+	printf ( "%s" , asctime ( timeStruct ) ); // transform breakdown time saved int timeStruct into null ended string and print it
 	return 0;	
 }
